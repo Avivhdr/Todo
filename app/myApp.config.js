1ebbfcf1aab31b157a412a83a66b1aacb43f5e13
Aviv Hadar <avivhdr@gmail.com>
2017-08-09 09:05:28 +0300

    remove state

--- app/myApp.config.js
+++ app/myApp.config.js
@@ -24,16 +24,12 @@
             template: '<multi-view></multi-view>'
         };
 
-        var testState = {
-            url: "/test",
-            template: '<test></test>'
-        };
 
         $stateProvider
             .state('main', mainState)
             .state('main.list', listState)
             .state('multiView', multiViewState)
-            .state('test', testState)
+
     }
 
 })();
