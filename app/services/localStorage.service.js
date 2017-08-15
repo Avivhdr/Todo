611aa67a4c5531114a35568d32c6305ac96f6c88
Aviv Hadar <avivhdr@gmail.com>
2017-08-15 18:42:35 +0300

    add Google Places API

--- app/services/localStorage.service.js
+++ app/services/localStorage.service.js
@@ -12,6 +12,7 @@
                     {
                         "id": 11,
                         "title": "Milk",
+                        "location": '',
                         "completed": false,
                         "details": "only 9% fat",
                         "created": "Wed May 06 2016"
@@ -19,6 +20,7 @@
                     {
                         "id": 12,
                         "title": "Bread",
+                        "location": '',
                         "completed": false,
                         "details": "Rye",
                         "created": "Wed May 04 2016"
@@ -26,6 +28,7 @@
                     {
                         "id": 13,
                         "title": "Cheese",
+                        "location": '',
                         "completed": false,
                         "details": "",
                         "created": "Wed May 07 2016"
@@ -33,6 +36,7 @@
                     {
                         "id": 14,
                         "title": "mayonnaise",
+                        "location": '',
                         "completed": false,
                         "details": "",
                         "created": "Wed May 09 2016"
@@ -46,6 +50,7 @@
                     {
                         "id": 21,
                         "title": "sun glasses",
+                        "location": '',
                         "completed": true,
                         "details": "the old ones",
                         "created": "Wed May 06 2016"
@@ -53,6 +58,7 @@
                     {
                         "id": 22,
                         "title": "swim pants",
+                        "location": '',
                         "completed": false,
                         "details": "",
                         "created": "Wed May 04 2016"
@@ -60,6 +66,7 @@
                     {
                         "id": 23,
                         "title": "towel",
+                        "location": '',
                         "completed": false,
                         "details": "always bring a towel",
                         "created": "Wed May 07 2016"
@@ -67,6 +74,7 @@
                     {
                         "id": 24,
                         "title": "book",
+                        "location": '',
                         "completed": false,
                         "details": "",
                         "created": "Wed May 09 2016"
@@ -80,6 +88,7 @@
                     {
                         "id": 31,
                         "title": "call mom",
+                        "location": '',
                         "completed": true,
                         "details": "ask how is she",
                         "created": "Wed May 06 2016"
@@ -87,6 +96,7 @@
                     {
                         "id": 32,
                         "title": "clean home",
+                        "location": '',
                         "completed": false,
                         "details": "windows also",
                         "created": "Wed May 04 2016"
@@ -94,6 +104,7 @@
                     {
                         "id": 33,
                         "title": "go to store (see shopping list)",
+                        "location": '',
                         "completed": false,
                         "details": "",
                         "created": "Wed May 07 2016"
@@ -101,6 +112,7 @@
                     {
                         "id": 34,
                         "title": "pick up mail",
+                        "location": '',
                         "completed": false,
                         "details": "open from 8:00 to 13:00",
                         "created": "Wed May 09 2016"
@@ -114,16 +126,34 @@
                     {
                         "id": 41,
                         "title": "Rick & Morty",
+                        "location": '',
                         "completed": true,
                         "details": "",
-                        "created": "Wed May 06 2016"
+                        "created": "Wed June 06 2016"
                     },
                     {
-                        "id": 22,
+                        "id": 42,
                         "title": "Planet Earth 2",
+                        "location": '',
                         "completed": false,
                         "details": "",
-                        "created": "Wed May 04 2016"
+                        "created": "Wed June 14 2016"
+                    },
+                    {
+                        "id": 43,
+                        "title": "Game of Thrones",
+                        "location": '',
+                        "completed": true,
+                        "details": "",
+                        "created": "Wed June 26 2016"
+                    },
+                    {
+                        "id": 44,
+                        "title": "Westworld",
+                        "location": '',
+                        "completed": false,
+                        "details": "Season 2",
+                        "created": "Wed June 04 2016"
                     }
                 ]
             }
